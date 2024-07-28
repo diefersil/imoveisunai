9271b2615d740bc07ff44fe6edf42a1b7506302d
diefersil <diefersil@gmail.com>
2024-07-28 03:31:59 -0300

    update

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -1,3 +1,14 @@
 <?php
-// code will goes here
-?>
+
+function my_theme_enqueue_styles() {
+    wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'parent-style' ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' )
+
+
+
