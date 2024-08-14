2e13d15e7c3ef02c02481cd4d0e251f1eda563ee
Diego Silva <diefersil@gmail.com>
2024-08-14 18:00:19 -0300

    update

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -1,3 +1,9 @@
 <?php
-// code will goes here
-?>
+function hide(){?>
+    <script type="text/javascript">
+        document.querySelector("input[name='facebook']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='linkedin']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='googleplus']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='tiktok']").parentNode.parentNode.style.display='none' 
+    </script>
+<? }
