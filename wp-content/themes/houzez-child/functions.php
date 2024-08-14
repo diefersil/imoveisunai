6506499ffb2b1212e36c6950622738c208de8f3a
Diego Silva <diefersil@gmail.com>
2024-08-14 19:20:10 -0300

    update

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -2,6 +2,21 @@
 add_action('wp_footer','hide');
 function hide(){?>
     <script type="text/javascript">
+
+        document.querySelector("input[name='telegram']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='line_id']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='fax_number']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='userlangs']").parentNode.parentNode.style.display='none' 
+        document.querySelector("input[name='specialties']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='service_areas']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='usermobile']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='userskype']").parentNode.parentNode.style.display='none' 
+        document.querySelector("input[name='youtube']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='vimeo']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='zillow']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='realtor_com']").parentNode.parentNode.style.display='none' 
+
+
         document.querySelector("input[name='facebook']").parentNode.parentNode.style.display='none'
         document.querySelector("input[name='linkedin']").parentNode.parentNode.style.display='none'
         document.querySelector("input[name='googleplus']").parentNode.parentNode.style.display='none'
