a3dd46cab5bbcb279174feb3e5f30057e35e4086
Diego Silva <diefersil@gmail.com>
2024-08-14 17:59:00 -0300

    update

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -1,3 +1,17 @@
 <?php
-// code will goes here
-?>
+
+
+
+
+
+
+add_action('wp_footer','hide')
+function hide(){?>
+    <script type="text/javascript">
+        document.querySelector("input[name='facebook']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='linkedin']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='googleplus']").parentNode.parentNode.style.display='none'
+        document.querySelector("input[name='tiktok']").parentNode.parentNode.style.display='none' 
+    </script>
+<? }
+
