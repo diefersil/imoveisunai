f16257e37de3041c90eb000930d49aa7461e3c05
Diego Silva <diefersil@gmail.com>
2025-04-17 15:37:10 -0300

    disable emojis

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -55,7 +55,29 @@ function gadc(){?>
     crossorigin="anonymous"></script>
 <?php }
 
-add_action('wp_head', 'hiltop_ads');
+/*add_action('wp_head', 'hiltop_ads');
 function hiltop_ads(){?>
     <meta name="352f73b99be1013e8252dcd9c84b82303813a2af" content="352f73b99be1013e8252dcd9c84b82303813a2af" />
-<?php }
+<?php }*/
+
+//Disable emojis in WordPress
+add_action( 'init', 'smartwp_disable_emojis' );
+
+function smartwp_disable_emojis() {
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+
+function disable_emojis_tinymce( $plugins ) {
+  if ( is_array( $plugins ) ) {
+    return array_diff( $plugins, array( 'wpemoji' ) );
+  } else {
+    return array();
+  }
+}
