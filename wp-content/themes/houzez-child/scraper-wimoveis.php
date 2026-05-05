03e2a0e69c17197d5b326ddc5be0796c6ebc1bce
Diego Fernandes <diefersil@gmail.com>
2026-05-05 15:55:52 -0300

    update

--- wp-content/themes/houzez-child/scraper-wimoveis.php
+++ wp-content/themes/houzez-child/scraper-wimoveis.php
@@ -1,137 +1,92 @@
 <?php
 
-$listUrl = "https://www.wimoveis.com.br/venda/imoveis/mg/unai";
-
-function getHtml($url) {
-    $ch = curl_init($url);
+function getMetaTagsFromUrl($url) {
+    $ch = curl_init();
 
     curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_TIMEOUT => 40,
-        CURLOPT_ENCODING => "",
-        CURLOPT_HTTPHEADER => [
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language: pt-BR,pt;q=0.9"
-        ]
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     ]);
 
     $html = curl_exec($ch);
     curl_close($ch);
 
-    return $html;
-}
-
-function getOgMeta($html, $property) {
-    $pattern1 = '/<meta[^>]+(?:property|name)=["\']' . preg_quote($property, '/') . '["\'][^>]+content=["\']([^"\']*)["\']/i';
-    $pattern2 = '/<meta[^>]+content=["\']([^"\']*)["\'][^>]+(?:property|name)=["\']' . preg_quote($property, '/') . '["\']/i';
-
-    if (preg_match($pattern1, $html, $m)) {
-        return html_entity_decode($m[1], ENT_QUOTES | ENT_HTML5, "UTF-8");
+    if (!$html) {
+        return [
+            'url' => $url,
+            'title' => null,
+            'description' => null,
+            'image' => null
+        ];
     }
 
-    if (preg_match($pattern2, $html, $m)) {
-        return html_entity_decode($m[1], ENT_QUOTES | ENT_HTML5, "UTF-8");
-    }
+    libxml_use_internal_errors(true);
+    $dom = new DOMDocument();
+    $dom->loadHTML($html);
+    $xpath = new DOMXPath($dom);
 
-    return null;
-}
-
-function extractJsonLd($html) {
-    preg_match_all(
-        '/<script[^>]+type=["\']application\/ld\+json["\'][^>]*>(.*?)<\/script>/is',
-        $html,
-        $matches
-    );
-
-    $items = [];
-
-    foreach ($matches[1] as $jsonText) {
-        $jsonText = trim(html_entity_decode($jsonText, ENT_QUOTES | ENT_HTML5, "UTF-8"));
-        $data = json_decode($jsonText, true);
-
-        if (!$data) {
-            continue;
+    // Função auxiliar
+    function getMeta($xpath, $property) {
+        $nodes = $xpath->query("//meta[@property='$property']");
+        if ($nodes->length > 0) {
+            return trim($nodes->item(0)->getAttribute('content'));
         }
-
-        if (isset($data[0])) {
-            foreach ($data as $item) {
-                $items[] = $item;
-            }
-        } else {
-            $items[] = $data;
-        }
+        return null;
     }
 
-    return $items;
-}
+    function getMetaName($xpath, $name) {
+        $nodes = $xpath->query("//meta[@name='$name']");
+        if ($nodes->length > 0) {
+            return trim($nodes->item(0)->getAttribute('content'));
+        }
+        return null;
+    }
 
-function extractProperties($jsonLdItems) {
-    $properties = [];
+    // OG tags
+    $title = getMeta($xpath, 'og:title');
+    $description = getMeta($xpath, 'og:description');
+    $image = getMeta($xpath, 'og:image');
+    $ogUrl = getMeta($xpath, 'og:url');
 
-    foreach ($jsonLdItems as $item) {
-        if (
-            isset($item["@type"]) &&
-            $item["@type"] === "SellAction" &&
-            isset($item["object"]["url"])
-        ) {
-            $object = $item["object"];
-
-            $properties[] = [
-                "titulo" => $object["name"] ?? null,
-                "descricao" => $object["description"] ?? null,
-                "url" => $object["url"] ?? null,
-                "area" => $object["floorSize"]["unitText"] ?? null,
-                "bairro" => $object["address"]["addressLocality"] ?? null,
-                "estado" => $object["address"]["addressRegion"] ?? null,
-                "cep" => $object["address"]["postalCode"] ?? null,
-                "endereco" => $object["address"]["streetAddress"] ?? null,
-                "latitude" => $object["geo"]["latitude"] ?? null,
-                "longitude" => $object["geo"]["longitude"] ?? null,
-            ];
+    // Fallbacks
+    if (!$title) {
+        $nodes = $xpath->query("//title");
+        if ($nodes->length > 0) {
+            $title = trim($nodes->item(0)->nodeValue);
         }
     }
 
-    return $properties;
-}
+    if (!$description) {
+        $description = getMetaName($xpath, 'description');
+    }
 
-$listHtml = getHtml($listUrl);
-
-$jsonLdItems = extractJsonLd($listHtml);
-$properties = extractProperties($jsonLdItems);
-
-$results = [];
-
-foreach ($properties as $property) {
-    $detailHtml = getHtml($property["url"]);
-
-    $results[] = [
-        "titulo_lista" => $property["titulo"],
-        "descricao_lista" => $property["descricao"],
-        "url_imovel" => $property["url"],
-        "area" => $property["area"],
-        "bairro" => $property["bairro"],
-        "estado" => $property["estado"],
-        "cep" => $property["cep"],
-        "endereco" => $property["endereco"],
-        "latitude" => $property["latitude"],
-        "longitude" => $property["longitude"],
-
-        "og_title" => getOgMeta($detailHtml, "og:title"),
-        "og_description" => getOgMeta($detailHtml, "og:description"),
-        "og_image" => getOgMeta($detailHtml, "og:image"),
-        "og_url" => getOgMeta($detailHtml, "og:url"),
+    return [
+        'url' => $ogUrl ?: $url,
+        'title' => $title,
+        'description' => $description,
+        'image' => $image
     ];
-
-    sleep(1);
 }
 
-header("Content-Type: application/json; charset=utf-8");
 
-echo json_encode([
-    "total" => count($results),
-    "imoveis" => $results
-], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+// 🔹 LISTA DE URLS
+$urls = [
+    "https://www.wimoveis.com.br/propriedades/fazenda-a-venda-em-unai-mg-664-ha-melhor-regiao-da-3020695945.html?n_src=Listado&n_pg=1&n_pos=1",
+    "https://www.zapimoveis.com.br/imovel/venda-terreno-lote-condominio-1-quarto-residencial-vitoria-unai-mg-241m2-id-2883562327/",
+    "https://imoveisunai.com.br/imoveis/aluga-se-casa-no-riviera-park-com-2-quartos/"
+];
+
+$resultados = [];
+
+foreach ($urls as $url) {
+    $resultados[] = getMetaTagsFromUrl($url);
+}
+
+// 🔹 Exibir resultado
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($resultados, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+?>
