e1418ca2353516f2ce194c86866a6552bc00ec53
Diego Fernandes <diefersil@gmail.com>
2026-06-04 19:35:14 -0300

    update

--- wp-content/themes/houzez-child/functions.php
+++ wp-content/themes/houzez-child/functions.php
@@ -1,45 +1,56 @@
 <?php
 
-/*Esconder campos desnecessários no painel do usuário*/
-add_action('wp_footer','hide');
-function hide(){
-    if(is_page('my-profile')){?>
+/*--------------------------------------------------------------*/
+/* Esconder campos desnecessários no painel do usuário */
+/*--------------------------------------------------------------*/
+
+add_action('wp_footer', 'hide');
+
+function hide() {
+    if (is_page('my-profile')) { ?>
         <script type="text/javascript">
             document.querySelector("input[name='telegram']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='line_id']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='fax_number']").parentNode.parentNode.style.display='none'
-            document.querySelector("input[name='userlangs']").parentNode.parentNode.style.display='none' 
+            document.querySelector("input[name='userlangs']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='specialties']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='service_areas']").parentNode.parentNode.style.display='none'
+
             //document.querySelector("input[name='usermobile']").parentNode.parentNode.style.display='none'
             //document.querySelector("input[name='title']").parentNode.parentNode.style.display='none'
+
             document.querySelector('[name=display_name]').parentNode.parentNode.style.display='none'
 
             document.querySelector("input[name='facebook']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='linkedin']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='googleplus']").parentNode.parentNode.style.display='none'
-            document.querySelector("input[name='tiktok']").parentNode.parentNode.style.display='none' 
+            document.querySelector("input[name='tiktok']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='twitter']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='pinterest']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='zillow']").parentNode.parentNode.style.display='none'
-            document.querySelector("input[name='userskype']").parentNode.parentNode.style.display='none' 
+            document.querySelector("input[name='userskype']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='youtube']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='vimeo']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='zillow']").parentNode.parentNode.style.display='none'
             document.querySelector("input[name='realtor_com']").parentNode.parentNode.style.display='none'
-            
+
             document.querySelector('[name=houzez-agency-package-security]').parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'
-            
+
             document.querySelectorAll('h2')[4].parentNode.parentNode.parentNode.style.display='none'
         </script>
     <?php }
 }
 
+
+/*--------------------------------------------------------------*/
+/* Alterar WhatsApp e telefone no single do imóvel */
+/*--------------------------------------------------------------*/
+
 add_action('wp_footer', 'single_wa');
 
 function single_wa() {
 
-    if ( ! is_single() ) {
+    if (!is_single()) {
         return;
     }
     ?>
@@ -92,55 +103,92 @@ function single_wa() {
 }
 
 
+/*--------------------------------------------------------------*/
+/* Google Analytics */
+/*--------------------------------------------------------------*/
+
 add_action('wp_head', 'ga');
-function ga(){?>
+
+function ga() { ?>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YSCEVGGKC3"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-    gtag('config', 'G-YSCEVGGKC3');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'G-YSCEVGGKC3');
     </script>
 <?php }
 
+
+/*--------------------------------------------------------------*/
+/* Google AdSense */
+/*--------------------------------------------------------------*/
+
 add_action('wp_head', 'gadc');
-function gadc(){?>
+
+function gadc() { ?>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0370842058394618"
-    crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
 <?php }
 
-/*add_action('wp_head', 'hiltop_ads');
-function hiltop_ads(){?>
-    <meta name="352f73b99be1013e8252dcd9c84b82303813a2af" content="352f73b99be1013e8252dcd9c84b82303813a2af" />
-<?php }*/
 
-//Disable emojis in WordPress
-add_action( 'init', 'smartwp_disable_emojis' );
+/*--------------------------------------------------------------*/
+/* Hiltop Ads - desativado */
+/*--------------------------------------------------------------*/
+
+/*
+add_action('wp_head', 'hiltop_ads');
+
+function hiltop_ads() { ?>
+    <meta name="352f73b99be1013e8252dcd9c84b82303813a2af" content="352f73b99be1013e8252dcd9c84b82303813a2af" />
+<?php }
+*/
+
+
+/*--------------------------------------------------------------*/
+/* Desativar emojis no WordPress */
+/*--------------------------------------------------------------*/
+
+add_action('init', 'smartwp_disable_emojis');
 
 function smartwp_disable_emojis() {
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
 }
 
-function disable_emojis_tinymce( $plugins ) {
-  if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-  } else {
+
+/*--------------------------------------------------------------*/
+/* Remover plugin de emoji do TinyMCE */
+/*--------------------------------------------------------------*/
+
+function disable_emojis_tinymce($plugins) {
+
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    }
+
     return array();
-  }
 }
 
 
+/*--------------------------------------------------------------*/
+/* Adicionar coluna "Contato" na listagem de imóveis do Houzez */
+/*--------------------------------------------------------------*/
 
-// Adiciona a coluna "Contato" na listagem de imóveis do Houzez
 add_filter('manage_property_posts_columns', function ($columns) {
 
     $new_columns = [];
@@ -158,7 +206,10 @@ add_filter('manage_property_posts_columns', function ($columns) {
 });
 
 
-// Mostra o valor do campo fave_contato na coluna
+/*--------------------------------------------------------------*/
+/* Mostrar valor do campo fave_contato na coluna Contato */
+/*--------------------------------------------------------------*/
+
 add_action('manage_property_posts_custom_column', function ($column, $post_id) {
 
     if ($column === 'fave_contato') {
