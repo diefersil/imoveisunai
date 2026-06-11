f19ba28adcdbf6f933548fa9a72a0c3a3f199416
Diego Fernandes <diefersil@gmail.com>
2026-06-10 22:53:40 -0300

    update

--- wp-content/themes/houzez-child/scraper-sites-config.php
+++ wp-content/themes/houzez-child/scraper-sites-config.php
@@ -176,7 +176,7 @@ $sites = [
             "preco" => ".//span[contains(@class,'ValorMoeda')]",
             "card_imagem_url" => ".//img[contains(@class,'BannerImage')]",
             "card_url" => ".//a",
-            "galeria" => ""
+            "galeria" => "//div[contains(@class,'overflow-image-gallery')]//img",
             "descricao" => "//div[contains(@class,'central_left')]"
         ]
     ]
