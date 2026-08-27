1fa6aec3b3e707fb79f7d72a7d3809862ef4c35f
Diego Fernandes <diefersil@gmail.com>
2026-08-27 13:52:06 -0300

    update

--- wp-content/themes/gu-theme/scraper-sites-config.php
+++ wp-content/themes/gu-theme/scraper-sites-config.php
@@ -31,7 +31,7 @@ $sites = [
         "numero_registros" => 20,
         "numero_maximo_por_url" => 10,
         "frequencia" => [
-            "tipo" => "horario",
+            "tipo" => "sempre",
             "horario_inicio" => "00:00",
             "horario_fim" => "02:00"
         ],
@@ -80,7 +80,7 @@ $sites = [
         "numero_registros" => 15,
         "numero_maximo_por_url" => 3,
         "frequencia" => [
-            "tipo" => "horario",
+            "tipo" => "sempre",
             "horario_inicio" => "02:00",
             "horario_fim" => "04:00"
         ],
@@ -127,7 +127,7 @@ $sites = [
         "numero_registros" => 30,
         "numero_maximo_por_url" => 10,
         "frequencia" => [
-            "tipo" => "horario",
+            "tipo" => "sempre",
             "horario_inicio" => "04:00",
             "horario_fim" => "06:00"
         ],
@@ -215,7 +215,7 @@ $sites = [
         "numero_registros" => 48,
         "numero_maximo_por_url" => 12,
         "frequencia" => [
-            "tipo" => "horario",
+            "tipo" => "sempre",
             "horario_inicio" => "22:00",
             "horario_fim" => "12:00"
         ],
