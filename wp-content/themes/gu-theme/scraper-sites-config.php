10eb9ec116ca8d9f7144aff8a20ab980c09bb567
Diego Fernandes <diefersil@gmail.com>
2026-09-09 00:00:42 -0300

    update

--- wp-content/themes/gu-theme/scraper-sites-config.php
+++ wp-content/themes/gu-theme/scraper-sites-config.php
@@ -222,9 +222,9 @@ $sites = [
         "numero_registros" => 48,
         "numero_maximo_por_url" => 15,
         "frequencia" => [
-            "tipo" => "sempre",
+            "tipo" => "horario",
             "horario_inicio" => "22:00",
-            "horario_fim" => "12:00"
+            "horario_fim" => "24:00"
         ],
         "verificar_string" => "",
         "seletores" => [
